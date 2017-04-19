90169d64255edd8ae5f304769ca9b670dec9b921
salahusama <salaheldin.akl@gmail.com>
2017-04-19 12:12:47 +0100

    sample OPENGL

--- FirstOpenGL/app/src/main/java/com/example/salah/firstopengl/MainActivity.java
+++ FirstOpenGL/app/src/main/java/com/example/salah/firstopengl/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.salah.firstopengl;
 
+import android.content.Context;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    class MyGLSurfaceView extends GLSurfaceView {
+
+        private final MyGLRenderer mRenderer;
+
+        public MyGLSurfaceView(Context context){
+            super(context);
+
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+
+            mRenderer = new MyGLRenderer();
+
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(mRenderer);
+        }
+    }
+
+
 }
