1b262f55c9292dd77b4dbf210916b5bba7dd85fb
salahusama <salaheldin.akl@gmail.com>
2017-04-20 05:00:49 +0100

    method ellipse not recognised

---
+++ AndroidProcessing/app/src/main/java/salah/android/processing/Ship.java
@@ -0,0 +1,19 @@
+package salah.android.processing;
+
+/**
+ * Created by salah on 20/04/17.
+ */
+
+public class Ship{
+    float x;
+    float y;
+
+    public Ship(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void display() {
+        ellipse(x, y, 50f, 50f);
+    }
+}
